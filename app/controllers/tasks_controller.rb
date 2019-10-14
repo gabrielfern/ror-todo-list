211db01a7c30c4c4bd46f3064005e4fc5df6fe80
Gabriel Fernandes <gabrielfernnd@gmail.com>
2019-10-13 23:08:03 -0300

    Two types of tasks, open tasks and finished tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,25 @@
 class TasksController < ApplicationController
+    #skip_before_action :verify_authenticity_token
+
     def index
-        @tasks = Task.all
+        @open_tasks = Task.where(finished: false).reorder('updated_at').reverse_order
+        @finished_tasks = Task.where(finished: true).reorder('updated_at').reverse_order
     end
 
     def create
         @task = Task.new(params.require(:task).permit(:description))
+        @task[:finished] = false
+        @task.save
+        redirect_to root_path
+    end
+
+    def update
+        @task = Task.find(params[:id])
+        if params[:finished] == 'true'
+            @task.finished = true
+        else
+            @task.finished = false
+        end
         @task.save
         redirect_to root_path
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_13_195957) do
+ActiveRecord::Schema.define(version: 2019_10_13_211534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
     t.string "description"
+    t.boolean "finished"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
